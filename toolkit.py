6eeb3062c4e250afbc26ba82c948c217d16c394e
Jason Acharya <acharyajason12@gmail.com>
2026-09-16 15:32:54 +0545

    Level 2: Completed with the implementation of duplicate id fix logic

--- toolkit.py
+++ toolkit.py
@@ -41,7 +41,7 @@ map_tens = {
 class Record:
     id: str
     name: str
-    age: str
+    age: int
     city: str
     score: str
 
@@ -54,7 +54,7 @@ class Record:
         return True
 
 
-# apply age specific rules
+# function to apply age specific rules
 def determine_age(str_age):
     try:
         age = int(str_age)
@@ -86,27 +86,58 @@ def determine_age(str_age):
             return ""
 
 
-# main function for data cleaning operations
+# function for data cleaning operations
 def clean_data(file):
+    cleaned_records = []
     reader = csv.DictReader(file)
     for row in reader:
-        age = determine_age(row.get("age"))
-        person = Record(row.get("id"), row.get("name"), age, row.get("city"), row.get("score"))
-
         # skip an entire row if it doesn't have any value
         if not any(value and value.strip() for value in row.values()):
             continue
+
+        age = determine_age(row.get("age"))
+        person = Record(row.get("id"), row.get("name"), age, row.get("city"), row.get("score"))
 
         # skip a row if even one field doesn't have value, except name
         if not person.is_valid():
             continue
 
+        cleaned_records.append(person)
+
+    return cleaned_records
+
+
+# function to handle duplicate ids
+def handle_id(records):
+    duplicate_id ={}
+    for current_record in records:
+        duplicate_id.update(
+            {current_record.id: [other_record for other_record in records if other_record.id == current_record.id]})
+
+    final_records = []
+    for key, value in duplicate_id.items():
+        num_of_val = len(value)
+
+        if num_of_val > 1:
+            first_record = value[0]
+
+            if all(record == first_record for record in value):
+                final_records.append(first_record)
+            # otherwise conflicting ID -> append nothing
+        else: 
+            final_records.append(value[0])
+
+    return final_records
+
 
 def main():
     file_path = "messy_people.csv"
     with open(file_path, "r", newline="") as file:
-        clean_data(file)
-
+        cleaned_records = clean_data(file)
+        final_records = handle_id(cleaned_records)
+        for record in final_records:
+            print(record)
+        print("Final Count: ", len(final_records))
 
 if __name__ == '__main__':
     main() 
